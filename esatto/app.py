1afecf19f95957af559227b48fe5158ab4648e09
SzymiYay <szymoon09@gmail.com>
2024-03-29 19:25:59 +0100

    Add logging and load patients data from file

--- esatto/app.py
+++ esatto/app.py
@@ -1,4 +1,29 @@
+from .data_loader.json_loader import get_patients_data
+from .data_loader.patients_loader import get_patients
+
+from .logger.model import CustomFormatter, MyLogger
+
+from typing import Final
+
 import logging 
 
 def main() -> None:
-    pass
+
+    """LOGGING"""
+    logger = MyLogger.get_logger()
+
+    """APP"""
+    logger.warning('STARTING APP')
+    FILENAME: Final[str] = 'esatto/data/patients.json'
+
+    patients_data = get_patients_data(FILENAME)
+    logger.info('Successfully loaded patients data from file')
+    print(patients_data)
+
+    patients = get_patients(patients_data)
+    logger.info('Successfully created patients')
+
+    for patient in patients:
+        print(patient)
+
+    logger.warning('ENDING APP')
