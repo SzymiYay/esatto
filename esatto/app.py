fd365c211efd7f23d13fcbdf5b05453193a94937
SzymiYay <szymoon09@gmail.com>
2024-03-29 20:23:43 +0100

    Add patient and address repositories and tables

--- esatto/app.py
+++ esatto/app.py
@@ -1,6 +1,13 @@
 from .data_loader.json_loader import get_patients_data
 from .data_loader.patients_loader import get_patients
 
+from .db.model.patient import Patient
+from .db.model.address import Address
+from .db.repo.connection import MySQLConnectionPoolBuilder
+from .db.repo.crud import CrudRepo
+from .db.repo.patient import PatientRepo
+from .db.repo.address import AddressRepo
+
 from .logger.model import CustomFormatter, MyLogger
 
 from typing import Final
@@ -8,6 +15,27 @@ from typing import Final
 import logging 
 
 def main() -> None:
+
+    connection_pool = MySQLConnectionPoolBuilder().build()
+
+    patient_repo = PatientRepo(connection_pool)
+    address_repo = AddressRepo(connection_pool)
+
+    patient_repo.create_table("id integer primary key auto_increment, first_name varchar(20) not null, last_name varchar(20) not null, PESEL varchar(20) not null")
+    address_repo.create_table("id integer primary key auto_increment, street varchar(50) not null, city varchar(50) not null, zip_code varchar(6) not null, foreign key (patient_id) references patients(id) on delete cascade on update cascade")
+
+    patient_repo.insert_many([
+        Patient(first_name='Jan', last_name='Kowalski', PESEL='00000000001'),
+        Patient(first_name='Anna', last_name='Nowak', PESEL='00000000002'),
+        Patient(first_name='Piotr', last_name='Kowalczyk', PESEL='00000000003'),
+    ])
+
+    address_repo.insert_many([
+        Address(street='Kowalska', city='Warszawa', zip_code='000001', patient_id=1),
+        Address(street='Nowa', city='Kraków', zip_code='000002', patient_id=2),
+        Address(street='Kowalska', city='Warszawa', zip_code='000001', patient_id=3)
+    ])
+
 
     """LOGGING"""
     logger = MyLogger.get_logger()
