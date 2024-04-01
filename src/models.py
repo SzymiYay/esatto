aa646a7358b4f19d5b7b414bfdbdb0bc795f0f74
SzymiYay <szymoon09@gmail.com>
2024-04-02 00:58:42 +0200

    Add models for User and Patient

---
+++ src/models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    disabled = Column(String)
+    time_created = Column(DateTime(timezone=True), server_default=func.now())
+    time_updated = Column(DateTime(timezone=True), onupdate=func.now())
+
+class Patient(Base):
+    __tablename__ = 'patients'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    PESEL = Column(String, unique=True, nullable=False)
+    city = Column(String(50), nullable=False)
+    street = Column(String(50), nullable=False)
+    zip_code = Column(String(10), nullable=False)
+    time_created = Column(DateTime(timezone=True), server_default=func.now())
+    time_updated = Column(DateTime(timezone=True), onupdate=func.now())
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    user = relationship('User')
